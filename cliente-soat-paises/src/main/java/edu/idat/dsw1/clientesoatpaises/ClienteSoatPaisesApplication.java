d4722c8684d62594ce2e6724d8dd63797819deee
Gicson22 <112468827+Gicson22@users.noreply.github.com>
2023-06-10 11:49:12 -0500

    Add files via upload

---
+++ cliente-soat-paises/src/main/java/edu/idat/dsw1/clientesoatpaises/ClienteSoatPaisesApplication.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.clientesoatpaises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSoatPaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSoatPaisesApplication.class, args);
+	}
+
+}
---
+++ cliente-soat-paises/src/test/java/edu/idat/dsw1/clientesoatpaises/ClienteSoatPaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.clientesoatpaises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSoatPaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
